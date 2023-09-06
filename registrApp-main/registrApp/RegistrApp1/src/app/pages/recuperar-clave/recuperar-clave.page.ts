7cd6bf0bfe292ae993d6f9dcf9c4d5999a0e85b3
alfrvillalobo <101344200+alfrvillalobo@users.noreply.github.com>
2023-09-05 22:58:26 -0300

    Add files via upload

---
+++ registrApp-main/registrApp/RegistrApp1/src/app/pages/recuperar-clave/recuperar-clave.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-recuperar-clave',
+  templateUrl: './recuperar-clave.page.html',
+  styleUrls: ['./recuperar-clave.page.scss'],
+})
+export class RecuperarClavePage implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+
+
+}
